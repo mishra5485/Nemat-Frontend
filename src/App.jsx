ecf9c69072ef48c83ba85f9daeb200138c460096
raj22001 <rajmahajan960@gmail.com>
2023-12-15 18:28:34 +0530

    add all routing both private and publich

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,33 @@ import './App.css'
 import Login from './component/Login'
 import ChangePassword from './component/ChangePassword'
 import CompanyDetails from './regestation/CompanyDetails'
+import { Route, Routes } from 'react-router-dom'
+import Home from './component/Home'
+import Error from './pages/Error'
+import {PrivateRoute }from './component/auth/PrivateRoute'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <CompanyDetails/> */}
-       <Login/>
-      {/*<ChangePassword/>*/}
+
+      <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route  path='/login' element={<Login/>} />
+        <Route  path='/companydetails' element={<CompanyDetails/>} />
+        
+        <Route  path='/login/changepassword' element={
+          <PrivateRoute>
+            <ChangePassword/>
+          </PrivateRoute>  
+          } 
+        />
+
+
+          {/* Error Router  */}
+          <Route path="*" element={<Error />} />  
+      </Routes>
     </>
   )
 }
