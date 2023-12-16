b978c3b48f52e13607b13510d265b48328297b36
raj22001 <rajmahajan960@gmail.com>
2023-12-16 16:16:49 +0530

    infinite Scrolling added

---
+++ src/style/InfiniteScrollImage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import FlowerPattern from "../assets/loginImages/FlowerPattern.png"
+import "./style.css"
+
+function InfiniteScrollImage() {
+
+   
+  return (
+       <div className="overflow-hidden">
+      <div className="animate-scrolling">
+        <img
+          src={FlowerPattern}
+          alt="FlowerPatternImage"
+          className="w-full animate-img"
+        />
+        <img
+          src={FlowerPattern}
+          alt="FlowerPatternImage"
+          className="w-full animate-img"
+        />
+         <img
+          src={FlowerPattern}
+          alt="FlowerPatternImage"
+          className="w-full animate-img"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default InfiniteScrollImage;
