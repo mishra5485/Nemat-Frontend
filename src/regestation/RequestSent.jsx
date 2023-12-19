f364fc6a28954b24f2994fd6f82df68d0c1c4cef
raj22001 <rajmahajan960@gmail.com>
2023-12-19 18:13:18 +0530

    mobile view and desktop view done

--- src/regestation/RequestSent.jsx
+++ src/regestation/RequestSent.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import loginBG from "../assets/loginImages/loginImage.png";
+import logo from "../assets/loginImages/nematEnterprisesLogo.png";
+import { MdDone } from "react-icons/md";
+import InfiniteScrollImage from "../style/InfiniteScrollImage";
+import FlowerPattern2 from "../assets/loginImages/FlowerPattern2.png";
 
-const RequestSent = () => {
+const PasswordUpdated = () => {
 
-    const MenuPageHandler = () => {
-         // console.log("New Page")
-  }
+  
 
 
   return (
+    <div className='w-full h-full object-cover md:flex md:w-full md:h-[100vh]'>
+          <div style={{ backgroundImage: `url(${loginBG})` , backgroundRepeat: 'no-repeat',  }} className= 'mobile:w-full sm:w-full  sm:h-[45vh] mobile::bg-center mobile:h-[40vh] mobile:bg-cover sm:bg-center mobile:bg-center sm:bg-cover sm:object-cover  bg-green-700 md:h-[100%]  md:bg-slate-600 md:min-w-[45%] flex-wrap object-cover -z-10 md:max-w-[80%] lg:w-[40%]' > 
+            <div className="flex w-[100%] mt-2 sm:mt-5 sm:  md:h-[20%] justify-center items-center   ">
+                <Link to={"/"}>
+                  <img src={logo} className="sm:w-[100%] z-10 mobile:h-[80px] mobile:w-[107px] sm:h-[90px] md:w-[150px] md:h-[105px] " alt="" />
+                </Link>
+              </div>  
+            </div>
 
-      <div className='w-full h-[300px] relative'>
-            <div className='relative'>
-                           {/* Header section in 3rd div */}
+            <div className='mobile:w-full mobile:h-[45px] sm:w-full sm:h-[45px] min-h-[5%] md:max-w-[4%] md:h-full md:mt-2  overflow-hidden  '>
+                <img
+                  src={FlowerPattern2}
+                  alt="FlowerPatternImage2"
+                  className="w-full h-[46px] sm:inline-block md:hidden"
+                />
 
-                </div>
+  {             /* Show FlowerPattern for md and larger screens */}
+               <InfiniteScrollImage  className="w-full h-full animate-img hidden mobile:hidden sm:hidden md:inline-block"/>
+            </div>
+         <div className='w-full h-full relative flex justify-center items-center' >
+                
+                    <div className='h-[60%] w-[90%] relative flex flex-col justify-center items-center'>
+                           <span className='p-3 border  rounded-full  bg-bg_green'><MdDone color="white" /></span>
+                           <h1 className="sm:mt-[26px] mobile:mt-[26px] font-roxborough text-[20px] leading-31 ">Request Sent Successfully</h1>
 
-                <div className='flex text-xs gap-2'>
-                    <h1 className='text-green-600 gap-x-1 cursor-pointer' ><span className='p-1 text-center border rounded-full ml-1 gap-x-2 bg-green-900'>1 </span> Company Details  {"->"}</h1>
-                    <h1  className="text-green-600 cursor-pointer"><span className= "p-1 text-center border rounded-full ml-1 gap-x-2 bg-green-900">2 </span> Company Details {"->"}</h1>
-                    <h1 className="text-green-600 cursor-pointer" ><span className='p-1 text-center border rounded-full ml-1 gap-x-2 bg-green-900'>3</span> Company Details </h1>
-                  </div>  
-                    <div className='relative flex flex-col justify-center items-center'>
-                           <span className='p-3 border  rounded-full  bg-green-400'>✔</span>
-                           <h1>Request Sent Successfully</h1>
-
-                           <p className='text-center'>
-                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quaerat commodi accusamus! Eligendi ea possimus repellendus blanditiis quam. Repudiandae, fuga.
+                           <p className='text-center mt-[30px] font-Marcellus text-16 leading-22 tracking-tight'>
+                              Lorem ipsum dolor sit amet consectetur. Viverra morbi molestie est venenatis turpis varius venenatis. Felis quisque congue massa consectetur eleifend. Enim velit fermentum nulla urna.
                            </p>
 
-                           <button className=' bg-green-600 p-3 rounded-2xl' onClick={MenuPageHandler}>
-                              <a className='text-2xl' href="/"> NEXT</a>
-                           </button>
+                            <Link to= {"/"} className="inline-flex mobile:w-[100%] sm:w-full md:w-[25%] h-[43px]  mobile:mt-[7%]  items-center justify-center  rounded-3xl bg-[#60713A]  leading-7 text-white font-Marcellus text-base  leading-17 tracking-normal text-center hover:animate-pulse hover:bg-green-700 transition-all duration-200 hover:text-white hover:bg-">
+                           <button className=''>
+                                NEXT
+                              </button>
+                           </Link>
 
                         </div>
         </div>
 
+   </div>
   )
 }
 
-export default RequestSent
+export default PasswordUpdated
