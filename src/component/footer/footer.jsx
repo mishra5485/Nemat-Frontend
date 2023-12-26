8b44677da8dd083faf9d7d0d2085db86cb2a46ae
raj22001 <rajmahajan960@gmail.com>
2023-12-26 17:59:44 +0530

    add desktop footer

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FooterImage from "../../assets/HomePage/FooterImage.png"
 import logo from "../../assets/loginImages/nematEnterprisesLogo.png"
 import { FaAngleDown , FaAngleUp  } from "react-icons/fa6";
@@ -42,6 +42,7 @@ const footer = () => {
 
    const [showAngle , setShowAngle] = useState({})
    const [selectedSeries, setSelectedSeries] = useState(null);
+   const [currentState , setCurrentState] = useState(window.innerWidth); 
 
    const toggleSubSeries = (seriesId) => {
     setSelectedSeries((prevSelectedSeries) => (prevSelectedSeries === seriesId ? null : seriesId));
@@ -63,34 +64,57 @@ const footer = () => {
     });
   };
 
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCurrentState(window.innerWidth);
+    }
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+  } , [currentState])
+
+  let isSmallScreen = currentState <= 760;
+
+
   return (
-    <div style={{ backgroundImage: `url(${FooterImage})` , backgroundRepeat: 'no-repeat',  }} className="w-full mobile:h-[900px]  sm:h-[900px] md:h-[600px] bg-center flex mobile:flex-col sm:flex-col justify-center items-center bg-Cream bg-cover">
-      <div className="w-[90%] h-full overflow-hidden " >
-         <div className='w-full mobile:h-[30%] mobile:mt-[30%] sm:mt-[27%] md:mt-[5%] mobile:flex sm:flex mobile:justify-center mobile:items-center sm:items-center '>
+    <div style={{ backgroundImage: `url(${FooterImage})` , backgroundRepeat: 'no-repeat',  }} className="w-full mobile:h-[950px]  sm:h-[900px] md:h-[600px] bg-center flex mobile:flex-col sm:flex-col justify-center items-center bg-Cream bg-cover">
+      <div className="w-[90%] h-full overflow-hidden  " >
+         <div className='w-full mobile:h-[30%] mobile:mt-[30%] sm:mt-[18%] md:mt-[8%] mobile:flex sm:flex mobile:justify-center mobile:items-center sm:items-center md:justify-start md:w-[30%]'>
                <img src={logo} className='w-[143px] h-[80px] ' />
          </div>  
 
 
-         <div className="mt-1 text-center text-white">
-            <h1 className="font-Abel text-2xl">Sign up for our newsletter</h1>
-            <p className="mt-2 font-Marcellus text-sm">Be the first to know about our special offers new product launches, and events</p>
+         <div className="mt-1 text-center text-white md:flex ">
+          <div className="md:text-start md:w-[35%]  md:flex md:flex-col md:justify-end">
 
-            <form className="mt-6">
-               <div className="flex border-b-2 ">
+            <h1 className="font-Abel text-2xl">Sign up for our newsletter</h1>
+            <p className="mt-2  font-Marcellus text-sm">Be the first to know about our special offers new product launches, and events</p>
+
+            <form className="mt-6 md:mt-8 ">
+               <div className="flex border-b-2">
                   <input 
                      type="email"
                      className="w-[80%] bg-transparent "
                      placeholder="Shop Categories"
                      />
-                     <span>Sign up</span>
+                     <span className="flex justify-between">Sign up</span>
                </div>
             </form>
+            </div>
 
-            <div className="mt-8">
+
+
+
+            <div className="md:w-[65%] md:-mt-9 md:h-full ml-[5%]">
+
+           
+            <div className="mt-8 md:flex md:justify-between">
                {footerDatas.map((footerData) => (
         <div key={footerData.id} className='flex flex-col mb-3'>
           <div className='flex justify-between items-center pb-1'
-            onClick={() => toggleSubSeries(footerData.id)}
+            onClick={() => isSmallScreen ? toggleSubSeries(footerData.id) : toggleSubSeries(footerData.id)}
           >
 
           <button
@@ -99,16 +123,16 @@ const footer = () => {
             >
             {footerData.series}
           </button>
-          {
+          {isSmallScreen  && (
              <p>{showAngle[footerData.id] ? <FaAngleUp size={20}/> : <FaAngleDown size={20}/> } </p>
-          }
+          )}
             </div>
 
           {selectedSeries === footerData.id && (
             <div
               className="z-10 "
             >
-              <ul className=" text-base pl-2 mb-2 font-Marcellus  text-start ">
+              <ul className=" text-base pl-2 mb-2 font-Marcellus  text-start md:hidden">
                 {footerData.subSeriesname.map((subSeries) => (
                   <li key={subSeries.SubSeriesname} className='hover:underline'>
                     <a
@@ -122,14 +146,32 @@ const footer = () => {
               </ul>
             </div>
           )}
+
+        
+          <div className="sm:hidden mobile:hidden md:flex">
+             <ul className=" text-base  mb-2 font-Marcellus  text-start ">
+                {footerData.subSeriesname.map((subSeries) => (
+                  <li key={subSeries.SubSeriesname} className='hover:underline'>
+                    <a
+                      href={subSeries.link}
+                      className=""
+                    >
+                      {subSeries.SubSeriesname}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+          </div>
+
         </div>
       ))}
             </div>
 
-            <div className="flex justify-between mobile:mt-8 sm:mt-5">
+            <div className="flex justify-between mobile:mt-8 sm:mt-5 md:justify-end items-center">
                <h1 className="w-[30%] text-start font-roxborough">Whole sale Purchase</h1>
                <button className="p-3 bg-white text-text_Color rounded-3xl font-Marcellus">LOGIN FOR BUSINESS</button>
             </div>
+             </div>
          </div>
       </div>
     </div>
