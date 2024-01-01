253c37fcf34ddc3c128c85c337b06500544dadba
raj22001 <rajmahajan960@gmail.com>
2024-01-01 17:59:12 +0530

    Add SlideBar and OutLet

--- nematAdmin/src/Component/Dashboard.jsx
+++ nematAdmin/src/Component/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import SlideBar from './sidebar/SlideBar'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
     <div>
-      hello
+        <SlideBar/>
+                <div className="p-4 sm:ml-64">
+
+          <Outlet/>
+        </div>
     </div>
   )
 }
