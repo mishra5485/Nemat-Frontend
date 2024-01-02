eebc6fa4598e8b3b12dc61eed3a0074b57db2369
raj22001 <rajmahajan960@gmail.com>
2024-01-02 18:12:13 +0530

    Added Global Schema for category Object

---
+++ nematAdmin/src/FormValidations/data.js
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+export const categoryObjectSchema = yup.object({
+  name: yup.string().min(2).required("Enter Category Name"),
+  metaTitle: yup.string().min(2).required("Enter Meta Title For Category"),
+  metaDesc: yup.string().min(2).required("Emter Meta Desc for Category"),
+  metaKeyword: yup.string().min(2).required("Enter Meta Keywords"),
+  slugUrl: yup.string().min(2).required("Enter slugUrl"),
+  cartDiscount: yup.string().min(2).required("Enter cartDiscount"),
+  bannerImageMobile: yup.string().required("Select the Picture "),
+  bannerImageDesktop: yup.string().required("Select the Picture "),
+});
