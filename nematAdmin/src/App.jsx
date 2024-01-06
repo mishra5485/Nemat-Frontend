22112ad4f397b010638f48f613b8d431c5f40f60
raj22001 <rajmahajan960@gmail.com>
2024-01-06 17:55:39 +0530

    Fragrance ,  Fragrance edit path added and also bannerSlider and bannerSlider Edit added

--- nematAdmin/src/App.jsx
+++ nematAdmin/src/App.jsx
@@ -1,63 +1,75 @@
-
-import './App.css'
-import Admin_login from './Component/Admin_login'
-import { Route, Routes } from 'react-router-dom'
-import Error from './Component/Error'
-import Dashboard from './Component/Dashboard'
-import { PrivateRoute } from './Component/auth/PrivateRoute'
-import Check from './Component/Check'
-import Category from './Component/sidebar/Category'
-import Sub_Category from "./Component/sidebar/Sub_Category"
-import Product from './Component/sidebar/Product'
-import Review from "./Component/sidebar/Review"
-import EditCategory from './Component/sidebar/EditCategory'
-import EditSub_Category from './Component/sidebar/EditSub_Category'
-import DiscountSlabe from './Component/sidebar/slabs/DiscountSlabe'
-import Edit_DiscountSlabe from './Component/sidebar/slabs/Edit_DiscountSlabe'
-import QuantityScheme from './Component/sidebar/slabs/QuantityScheme'
-import Edit_QuantityScheme from './Component/sidebar/slabs/Edit_QuantityScheme'
-import PromoHeader from './Component/sidebar/website/PromoHeader'
+import "./App.css";
+import Admin_login from "./Component/Admin_login";
+import { Route, Routes } from "react-router-dom";
+import Error from "./Component/Error";
+import Dashboard from "./Component/Dashboard";
+import { PrivateRoute } from "./Component/auth/PrivateRoute";
+import Check from "./Component/Check";
+import Category from "./Component/sidebar/Category";
+import Sub_Category from "./Component/sidebar/Sub_Category";
+import Product from "./Component/sidebar/Product";
+import Review from "./Component/sidebar/Review";
+import EditCategory from "./Component/sidebar/EditCategory";
+import EditSub_Category from "./Component/sidebar/EditSub_Category";
+import DiscountSlabe from "./Component/sidebar/slabs/DiscountSlabe";
+import Edit_DiscountSlabe from "./Component/sidebar/slabs/Edit_DiscountSlabe";
+import QuantityScheme from "./Component/sidebar/slabs/QuantityScheme";
+import Edit_QuantityScheme from "./Component/sidebar/slabs/Edit_QuantityScheme";
+import PromoHeader from "./Component/sidebar/website/PromoHeader";
+import BannerSlider from "./Component/sidebar/website/BannerSlider";
+import VerifyUser from "./Component/sidebar/registerUser/verifyUser";
+import Edit_BannerSlider from "./Component/sidebar/website/Edit_BannerSlider";
+import Fragrance from "./Component/sidebar/slabs/Fragrance";
+import Edit_Fragrance from "./Component/sidebar/slabs/Edit_Fragrance";
 
 function App() {
-
-
   return (
     <>
       <Routes>
-          <Route  path='/' element={<Admin_login/>}/>
-           <Route path="*" element={<Error />} />  
+        <Route path="/" element={<Admin_login />} />
+        <Route path="*" element={<Error />} />
+        <Route path="verifyuser/:_id" element={<VerifyUser />} />
 
-         
-            <Route path='/dashboard' element={<Dashboard/>}> 
-                <Route  path='check' element={
-                  
-                      <Check/>
-                    } 
-                  />  
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="check" element={<Check />} />
 
-                    {/* Category with Category Edit after Creation  */}
-                    <Route path='category' element={<Category />}/>
-                      <Route path='category/edit/:_id' element={<EditCategory />} />
-                    
-                   {/* Sub_Category with Category Edit after Creation  */}
-                  <Route path='sub_category' element={<Sub_Category/>}/>
-                    <Route path='sub_category/sub_edit/:_id' element={<EditSub_Category/>}/>
+          {/* Category with Category Edit after Creation  */}
+          <Route path="category" element={<Category />} />
+          <Route path="category/edit/:_id" element={<EditCategory />} />
 
-                  <Route path='product' element={<Product/>}/>
-                  <Route path='product_review' element={<Review/>}/>
-                  
-                  <Route path='discountSlabe' element={<DiscountSlabe/>}/>
-                    <Route path='discountSlabe/Slabs_edit/:_id' element={<Edit_DiscountSlabe/>}/>
+          {/* Sub_Category with Category Edit after Creation  */}
+          <Route path="sub_category" element={<Sub_Category />} />
+          <Route
+            path="sub_category/sub_edit/:_id"
+            element={<EditSub_Category />}
+          />
 
-                  <Route path='quantityscheme' element={<QuantityScheme/>}/>
-                    <Route path='quantityscheme/scheme_edit/:_id' element={<Edit_QuantityScheme/>}/>
+          <Route path="product" element={<Product />} />
+          <Route path="product_review" element={<Review />} />
 
-                  <Route path='website/promoHeader' element={<PromoHeader/>}/>
-           </Route>
-           
-      </Routes>  
+          <Route path="discountSlabe" element={<DiscountSlabe />} />
+          <Route
+            path="discountSlabe/Slabs_edit/:_id"
+            element={<Edit_DiscountSlabe />}
+          />
+
+          <Route path="quantityscheme" element={<QuantityScheme />} />
+          <Route
+            path="quantityscheme/scheme_edit/:_id"
+            element={<Edit_QuantityScheme />}
+          />
+          
+          <Route path="fragrance"  element={<Fragrance/>}/>
+            <Route path="fragrance/fragrance_edit/:_id" element={<Edit_Fragrance/>}/>
+
+          <Route path="website/promoHeader" element={<PromoHeader />} />
+
+          <Route path="website/bannerSlider" element={<BannerSlider />} />
+            <Route path="website/bannerSlider/edit_bannerslider/:_id" element={<Edit_BannerSlider/>}/>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
