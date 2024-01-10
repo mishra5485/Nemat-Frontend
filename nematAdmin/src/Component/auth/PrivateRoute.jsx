0806fdf98c5d8a9e2650bfb54ac4dfd9da7cd866
raj22001 <rajmahajan960@gmail.com>
2024-01-10 17:19:15 +0530

    Need to Change in Private Routes

--- nematAdmin/src/Component/auth/PrivateRoute.jsx
+++ nematAdmin/src/Component/auth/PrivateRoute.jsx
@@ -5,12 +5,11 @@ import { Navigate } from 'react-router-dom'
 
 export const PrivateRoute = ({children }) => {
 
-   let {user} = useSelector((store) => store.profile)
-   console.log(user)
+  const { user } = useSelector((store) => store.profile);
+  console.log(user)
 
-     if (user === null) {
-      console.log("done")
-    return <Navigate to='/' />;
+  if (!user) {
+    return <Navigate to="/" />;
   }
 
   return children;
