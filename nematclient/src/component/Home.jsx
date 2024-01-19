b4eac46dfc7f67257d4ae243b2225511a9b237ac
raj22001 <rajmahajan960@gmail.com>
2024-01-19 15:09:00 +0530

    New Package Added for Slider and Banner Slider is Added

--- nematclient/src/component/Home.jsx
+++ nematclient/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import RightToLeftText from "../style/RightToLeftText";
 import { IoSearchOutline } from "react-icons/io5";
 import { AiOutlineShopping } from "react-icons/ai";
@@ -21,28 +21,30 @@ import Footer from "./footer/footer";
 import "../App.css";
 import axios from "axios";
 import ProductHeader from "./common/ProductHeader";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 760px)" });
-  const [showdata, SetShowData] = useState([]);
+  const [firstApiCall , setFirstApiCall] = useState(true)
   const [showAngle, setShowAngle] = useState({});
   const [showNavbar, SetShowNavbar] = useState(true);
-  const [isShopHovered, setShopHovered] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
-  const [isNavVisible, setIsNavVisible] = useState(false);
   const [categoryData, setCategoryData] = useState([]);
   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
   const [loading, setLoading] = useState(true);
-  const [DiscountSchemeData , setDiscountSchemeData] = useState([])
+  const [BannerData , setBannerData] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [Dssprays , setDsSprays] = useState()
+  const [agarbattisDs , setAgarbattisDs] = useState();
   const navigate = useNavigate();
-
-  
+  const sliderRef = useRef(null);
+  const baseURL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
   const mobileNavbar = () => {
     SetShowNavbar(!showNavbar);
   };
-
-
 
   const details = [
     {
@@ -86,22 +88,44 @@ const Home = () => {
     });
   };
 
-  useEffect(() => {
-    // const handleScroll = () => {
-    //   const currentScrollPos = window.pageYOffset;
-    //   const isScrolledUp = prevScrollPos > currentScrollPos;
+  // useEffect(() => {
+  //   // const handleScroll = () => {
+  //   //   const currentScrollPos = window.pageYOffset;
+  //   //   const isScrolledUp = prevScrollPos > currentScrollPos;
 
-    //   setIsNavVisible(isScrolledUp || currentScrollPos < 0); // Show navbar when at the top
+  //   //   setIsNavVisible(isScrolledUp || currentScrollPos < 0); // Show navbar when at the top
 
-    //   setPrevScrollPos(currentScrollPos);
-    // };
+  //   //   setPrevScrollPos(currentScrollPos);
+  //   // };
 
-    getAllHomePageData();
-    // window.addEventListener('scroll', handleScroll);
-    // return () => {
-    //   window.removeEventListener('scroll', handleScroll);
-    // };
-  }, []);
+   
+  //   // window.addEventListener('scroll', handleScroll);
+  //   // return () => {
+  //   //   window.removeEventListener('scroll', handleScroll);
+  //   // };
+  // }, []);
+
+   useEffect(() => {
+    // Automatically change banner after 10 seconds
+    const changeBanner = () => {
+      if (sliderRef.current) {
+        const nextSlide = (currentSlide + 1) % BannerData.length;
+        sliderRef.current.slickGoTo(nextSlide);
+      }
+    };
+
+    const timeoutId = setTimeout(changeBanner, 3000);
+
+    
+
+    if(firstApiCall){
+       getAllHomePageData();
+    }
+
+    return () => clearTimeout(timeoutId);
+
+  }, [currentSlide, BannerData.length]);
+
 
   const getAllHomePageData = async () => {
     try {
@@ -112,11 +136,13 @@ const Home = () => {
       console.log("allDataResponse.data", allDataResponse.data);
 
       if (allDataResponse.status === 200) {
-        SetShowData(allDataResponse.data);
         setCategoryData(allDataResponse.data.CategoryData);
-        setDiscountSchemeData(allDataResponse.data.CartDiscountSchemeData
-)
+        const checkDS = allDataResponse.data.CartDiscountSchemeData
+        setDsSprays(checkDS[0])
+        setAgarbattisDs(checkDS[1])
+        setBannerData(allDataResponse.data.BannerData)
         setLoading(false);
+        setFirstApiCall(false)
       }
     } catch (error) {
       if (error.response) {
@@ -136,12 +162,34 @@ const Home = () => {
         }
       }
     }
+    
   };
 
-  console.log("show data =====>", showdata);
-  console.log("Discount SlabeDATA ==== > " , DiscountSchemeData) 
+  // console.log("show data =====>", showdata)
+  
 
- 
+
+
+ const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (index) => setCurrentSlide(index),
+    autoplay: true,
+    autoplaySpeed: 10000, // 10 seconds
+    beforeChange: (current, next) => {
+    // If transitioning from the last to the first slide
+    if (next === 0 && current === BannerData.length - 1) {
+      // Move the first slide to the right
+      const slider = sliderRef.current;
+      if (slider) {
+        slider.slickGoTo(current + 1);
+      }
+    }
+  },
+  };
 
   return (
     <div className="mt-0  overflow-auto custom-scrollbar">
@@ -195,16 +243,16 @@ const Home = () => {
                     <div className="z-10 ">
                       <ul className="py-2 text-base pl-3 font-Marcellus text-text_Color  ">
                         {category.SubCategories &&
-                        category.SubCategories.map((subcategories) => (
-                          <li
-                            key={subcategories._id}
-                            className="hover:underline"
-                          >
-                            <Link to= {subcategories.link} className="">
-                              {subcategories.Name}
-                            </Link>
-                          </li>
-                        ))}
+                          category.SubCategories.map((subcategories) => (
+                            <li
+                              key={subcategories._id}
+                              className="hover:underline"
+                            >
+                              <Link to={subcategories.link} className="">
+                                {subcategories.Name}
+                              </Link>
+                            </li>
+                          ))}
                       </ul>
                     </div>
                   )}
@@ -225,12 +273,7 @@ const Home = () => {
             </div>
           </div>
         ) : (
-          <div
-            style={{
-              backgroundImage: `url(${isMobile ? MobileBG : DesktopBG})`,
-            }}
-            className="w-full h-[90vh]  mobile:bg-cover sm:bg-center"
-          >
+          <div>
             <div
               className={`relative py-4 check ${
                 isHovered ? "bg-LightCream" : ""
@@ -261,8 +304,12 @@ const Home = () => {
                       )}
                     </span>
                   </h1>
-                  <h1 className="hover:underline cursor-pointer font-semibold">ABOUT</h1>
-                  <h1 className="hover:underline cursor-pointer font-semibold ">CONTACT</h1>
+                  <h1 className="hover:underline cursor-pointer font-semibold">
+                    ABOUT
+                  </h1>
+                  <h1 className="hover:underline cursor-pointer font-semibold ">
+                    CONTACT
+                  </h1>
                 </div>
 
                 <div className="w-[143px] h-[66px]">
@@ -287,29 +334,32 @@ const Home = () => {
             </div>
             {isHovered ? (
               <div
-                className="absolute w-full bg-LightCream  flex justify-center"
+                className="absolute w-full bg-LightCream  flex justify-center z-20"
                 onMouseLeave={() => setIsHovered(false)}
               >
                 <div className="flex w-[90%] justify-between ">
                   {categoryData.map((category) => (
                     <div key={category._id} className="flex  flex-col">
                       <div className="flex justify-between items-center p-3 cursor-pointer">
-                        <h1 className=" font-Marcellus text-text_Color text-2xl hover:underline" style={{ minHeight: "3em" }}>
-                          {category.Name} 
+                        <h1
+                          className=" font-Marcellus text-text_Color text-2xl hover:underline"
+                          style={{ minHeight: "3em" }}
+                        >
+                          {category.Name}
                         </h1>
                       </div>
 
                       <div className="pl-3 h-[70%]">
                         <ul className="py-2 text-base font-Marcellus text-text_Color ">
                           {category.SubCategories &&
-                        category.SubCategories.map((subcategories) => (
-                            <li
-                              key={subcategories._id}
-                              className="hover:underline cursor-pointer"
-                            >
-                              {subcategories.Name}
-                            </li>
-                          ))}
+                            category.SubCategories.map((subcategories) => (
+                              <li
+                                key={subcategories._id}
+                                className="hover:underline cursor-pointer"
+                              >
+                                {subcategories.Name}
+                              </li>
+                            ))}
                         </ul>
                       </div>
                     </div>
@@ -319,29 +369,61 @@ const Home = () => {
             ) : (
               ""
             )}
-
-            <div className="mobile:w-[100%] mobile:h-[40%] mobile:flex mobile:flex-col mobile:justify-evenly mobile:items-center md:justify-center md:items-start md:h-[80%] md:w-[80%] md:mx-auto md:text-start md:gap-y-4">
-              <h1 className="font-Abel  md:text-start font-[25px] text-2xl">
-                ATTAR 96
-              </h1>
-              <h1 className="font-Abel font-[35px] text-3xl overflow-hidden">
-                LONDON SERIES
-              </h1>
-
-              <p className="w-[80%] md:w-[350px] md:text-start mobile:text-center font-Marcellus text-base">
-                Most fonts have a particular weight which corresponds to one of
-                the numbers in Common weight name mapping. However some fonts,
-                called variable fonts.
-              </p>
-
-              <button className="w-[137px] h-[43px] bg-bg_green rounded-3xl">
-                SHOP NOW
-              </button>
-            </div>
           </div>
         )}
       </header>
-
+      {loading ? (
+        <p>Loading </p>
+      ) : (
+        <div className="relative">
+      <Slider {...settings} ref={sliderRef} className="overflow-hidden z-10">
+        {BannerData.map((bannerItem) => (
+          <div
+            key={bannerItem._id}
+            className="w-full h-[85vh] object-cover sm:bg-center overflow-hidden relative"
+          >
+            <img
+              src={`${baseURL}/${
+                isMobile ? bannerItem.MobilebannerImage : bannerItem.DesktopbannerImage
+              }`}
+              alt={bannerItem.Heading}
+              className="w-full h-full object-cover"
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
+            {/* <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+              <h1 className="font-Abel md:text-start font-[25px] text-2xl">
+                {bannerItem.Heading}
+              </h1>
+              <h1 className="font-Abel font-[35px] text-3xl overflow-hidden">
+                {bannerItem.SubHeading}
+              </h1>
+              <p className="w-[80%] md:w-[350px] md:text-start mobile:text-center font-Marcellus text-base">
+                {bannerItem.Description}
+              </p>
+              <button className="w-[137px] h-[43px] bg-bg_green rounded-3xl">
+                SHOP NOW
+              </button>
+            </div> */}
+          </div>
+        ))}
+      </Slider>
+      <div className="absolute bottom-4 left-0 right-0 flex justify-center">
+        <div className="flex space-x-2 ">
+          {[...Array(3)].map((_, dotIndex) => (
+            <span
+              key={dotIndex}
+              className={`h-2 w-2 rounded-full bg-black ${
+                dotIndex === currentSlide % 3 ? "opacity-100" : "opacity-50"
+              }`}
+              onClick={() =>
+                sliderRef.current && sliderRef.current.slickGoTo(currentSlide + dotIndex)
+              }
+            ></span>
+          ))}
+        </div>
+      </div>
+        </div>
+      )}
       <div>
         {loading ? (
           <p>Loading </p>
@@ -395,7 +477,7 @@ const Home = () => {
         )}
       </div>
 
-      {/* 
+      {/*  
       <Attars/>
       <RightToLeftanm image={FlowerPattern2}/>
       <Rollons/>
@@ -403,11 +485,17 @@ const Home = () => {
       <Non_Alcoholic/>
       <RightToLeftanm image={FlowerPattern2}/>
       <Agarabtti/> */}
-      <Discountslabe />
-      {loading ? (
-          <p>Loading </p>
-        ) : (<Footer categoryData={categoryData} /> )
-      }
+      {console.log("Dssprays ==>" , Dssprays)}
+      {console.log("agarbattisDs ==>" , agarbattisDs)}
+      {loading ? <p>Loading </p> : (
+        <>
+        {/* {console.log("Dssprays ==>" , Dssprays)}
+        {console.log("agarbattisDs ==>" , agarbattisDs)} */}
+      <Discountslabe  Dssprays = {Dssprays} agarbattisDs={agarbattisDs}/>
+      </>
+      )}
+        
+      {loading ? <p>Loading </p> : <Footer categoryData={categoryData} />}
     </div>
   );
 };
