fc8dfbcc610374156ff41780c96448e15d037015
raj22001 <rajmahajan960@gmail.com>
2024-02-16 11:44:01 +0530

    Search functionality added For Vendors

--- nematAdmin/src/Component/sidebar/vendors/Vendors.jsx
+++ nematAdmin/src/Component/sidebar/vendors/Vendors.jsx
@@ -9,6 +9,7 @@ const Vendors = () => {
   const [showform, setShowForm] = useState();
   const [loading, setLoading] = useState(true);
   const [allvandors, setvandors] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   const navigate = useNavigate();
 
@@ -23,6 +24,7 @@ const Vendors = () => {
       );
 
       setvandors(response.data);
+      setFilteredData(response.data)
       setLoading(false);
     } catch (error) {
       if (error.response) {
@@ -182,6 +184,13 @@ const Vendors = () => {
   };
 
   useEffect(() => {}, [allvandors]);
+
+   const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase(); 
+    const filtered = allvandors.filter(item => item.Name.toLowerCase().includes(searchTerm)); 
+    setFilteredData(filtered);
+  };
+
 
   return (
     <div>
@@ -366,6 +375,7 @@ const Vendors = () => {
                         type="text"
                         placeholder="Search for products"
                         required=""
+                         onChange={(event) => handleSearch(event)}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       />
                     </div>
@@ -424,10 +434,10 @@ const Vendors = () => {
                         </tr>
                       </thead>
 
-                      {!allvandors || allvandors.length === 0 ? (
+                      {!filteredData || filteredData.length === 0 ? (
                         <p>NO data Found</p>
                       ) : (
-                        allvandors.map((item) => (
+                        filteredData.map((item) => (
                           <tbody key={item._id}>
                             <tr className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
                               <td className="p-4 w-4">
