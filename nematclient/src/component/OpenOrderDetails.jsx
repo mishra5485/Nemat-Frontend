39a4bb2d06b4b2a72308233891b6f737f7e5c039
raj22001 <rajmahajan960@gmail.com>
2024-02-17 16:24:04 +0530

    add address common function created

--- nematclient/src/component/OpenOrderDetails.jsx
+++ nematclient/src/component/OpenOrderDetails.jsx
@@ -1,13 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
-import { formattedAmount } from "./common/FormatAmount";
-import { MdOutlineFileDownload } from "react-icons/md";
-import { FaCircleCheck } from "react-icons/fa6";
-import { IoRemoveOutline } from "react-icons/io5";
-import toast, { Toaster } from "react-hot-toast";
-import { FaLongArrowAltRight } from "react-icons/fa";
 import OrderDetailsData from "./common/orderDetailsData";
 
 
@@ -70,7 +63,6 @@ const OpenOrderDetails = () => {
   return (
     // <div className="sm:mt-4 mobile:mt-4 md:h-auto md:ml-[7%]   ">
       <div>
-      <Toaster />
       {loading ? (
         <p>Loaddding....</p>
       ) : (
