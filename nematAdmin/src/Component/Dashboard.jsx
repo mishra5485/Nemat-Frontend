b1c3064876f93d6524a32e8d5ef7f190395cb9c1
raj22001 <rajmahajan960@gmail.com>
2024-02-21 17:29:36 +0530

    Dashboard Componant added

--- nematAdmin/src/Component/Dashboard.jsx
+++ nematAdmin/src/Component/Dashboard.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import SlideBar from './sidebar/SlideBar'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import SlideBar from './sidebar/SlideBar';
+import { Outlet } from 'react-router-dom';
+import DashboardComponantData from './DashboardComponantData';
 
 const Dashboard = () => {
+  const location = useLocation();
+  const isDashboardPage = location.pathname === '/dashboard'; // Assuming '/dashboard' is the path of your Dashboard page
+
   return (
     <div className='h-[100%]'>
-        <SlideBar/>
-                <div className="p-4 sm:ml-64">
-          <Outlet/>
-        </div>
+      <SlideBar/>
+      <div className="p-4 sm:ml-64">
+        <Outlet/>
+        {isDashboardPage && <DashboardComponantData/>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
