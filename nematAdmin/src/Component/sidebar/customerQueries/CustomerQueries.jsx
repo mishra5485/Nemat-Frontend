a781d1bc4454a547edad913ee9bbb5142a70a542
raj22001 <rajmahajan960@gmail.com>
2024-02-23 10:18:33 +0530

    Change Title

--- nematAdmin/src/Component/sidebar/customerQueries/CustomerQueries.jsx
+++ nematAdmin/src/Component/sidebar/customerQueries/CustomerQueries.jsx
@@ -68,7 +68,7 @@ const CustomerQueries = () => {
     <div>
       <h1 className="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
         <span className="text-transparent bg-clip-text bg-gradient-to-r  to-emerald-600 from-sky-400">
-          Order Management
+          Customer Queries
         </span>
       </h1>
       <Toaster />
