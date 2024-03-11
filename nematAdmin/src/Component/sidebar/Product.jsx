972d1c74729afef2219a306a7ff692a46c8fccc4
raj22001 <rajmahajan960@gmail.com>
2024-03-11 14:57:54 +0530

    Product Global Search added and after cut display old data

--- nematAdmin/src/Component/sidebar/Product.jsx
+++ nematAdmin/src/Component/sidebar/Product.jsx
@@ -6,6 +6,7 @@ import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import LoadingSpinner from "../common/LoadingSpinner";
+import SearchProduct from "./SearchProduct";
 
 const Product = () => {
   const [AllCategoryData, setAllCategoryData] = useState();
@@ -1015,22 +1016,7 @@ const Product = () => {
             <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
               <div className="flex flex-col md:flex-row items-stretch md:items-center md:space-x-3 space-y-3 md:space-y-0 justify-between mx-4 py-4 border-t dark:border-gray-700">
                 <div className="w-full md:w-1/2">
-                  <form className="flex items-center">
-                    <label htmlFor="simple-search" className="sr-only">
-                      Search
-                    </label>
-                    <div className="relative w-full">
-                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-                      <input
-                        type="text"
-                        id="simple-search"
-                        placeholder="Search for products"
-                        required=""
-                        onChange={(event) => handleSearch(event)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      />
-                    </div>
-                  </form>
+                  <SearchProduct allProductData={allProductData} setFilteredData={setFilteredData}/>
                 </div>
                 <div>
                   <select
---
+++ nematAdmin/src/Component/sidebar/SearchProduct.jsx
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const SearchProduct = ({ allProductData, setFilteredData }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+    }, 700);
+
+    return () => clearTimeout(delay);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const payload = {
+          keyword: debouncedSearchTerm,
+        };
+
+        let response = await axios.post(
+          `${import.meta.env.VITE_REACT_APP_BASE_URL}/product/get/searchData`,
+          payload
+        );
+
+        setFilteredData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching search results:", error);
+        setLoading(false);
+      }
+    };
+
+    // Fetch data only if search term is not empty
+    if (debouncedSearchTerm !== "") {
+      fetchData();
+    } else {
+      // If search term is empty, reset filtered data to initial data
+      setFilteredData(allProductData);
+    }
+  }, [debouncedSearchTerm]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div>
+      <form className="flex items-center">
+        <label htmlFor="simple-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-full">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+          <input
+            type="text"
+            placeholder="Search"
+            id="camponeyname"
+            value={searchTerm}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchProduct;
