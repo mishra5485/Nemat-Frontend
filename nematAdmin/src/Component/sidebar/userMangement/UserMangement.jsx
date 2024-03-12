436c1e4f18ce8372dd704199b2ecd4e571b27146
raj22001 <rajmahajan960@gmail.com>
2024-03-12 17:08:43 +0530

    Pagination  Global Search and Drop Selection Added

--- nematAdmin/src/Component/sidebar/userMangement/UserMangement.jsx
+++ nematAdmin/src/Component/sidebar/userMangement/UserMangement.jsx
@@ -1,40 +1,180 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import UserNewComponent from "./UserNewComponent";
+import toast, { Toaster } from "react-hot-toast";
+
 
 const UserManagement = () => {
   const [userDatas, setUserDatas] = useState();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [selectedArray, setSelectedArray] = useState(null);
   const [apiData, setApiData] = useState(null);
+  const [originalData, setOriginalData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [maxData, setMaxData] = useState(1);
+  const [flagSend, setFlagSend] = useState("1");
+  const [flagGetData, setFlagGetData] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const pageSize = 10;
 
   useEffect(() => {
     getAllUserData();
-  }, []);
+  }, [flagSend, currentPage]);
+
+  const companyDropDown = [
+    {
+      id: 1,
+      value: 1,
+      title: "ActiveUsers",
+    },
+    {
+      id: 2,
+      value: 3,
+      title: "EmailVerified",
+    },
+    {
+      id: 3,
+      value: 4,
+      title: "EmailNotVerified",
+    },
+    {
+      id: 4,
+      value: 2,
+      title: "DisabledUsers",
+    },
+  ];
 
   const getAllUserData = async () => {
+    const statusCheck = parseInt(flagSend);
+    const payload = {
+      flag: statusCheck,
+    };
+
     try {
-      const userData = await axios.get(
-        `${import.meta.env.VITE_REACT_APP_BASE_URL}/users/customer/getall`
+      const userData = await axios.post(
+        `${
+          import.meta.env.VITE_REACT_APP_BASE_URL
+        }/users/customer/getall/${pageSize}/${currentPage}`,
+        payload
       );
-      setApiData(userData.data);
-      // Set the selectedArray to the first array name by default
-      setSelectedArray(Object.keys(userData.data)[0]);
+
+      console.log(userData.data);
+
+      setApiData(userData.data.CustomerData);
+      setOriginalData(userData.data.CustomerData);
+
+      if (currentPage == 0) {
+        setMaxData(Math.ceil(response.data.otalCustomerDataLength / pageSize));
+      }
+
       setLoading(false);
+
     } catch (error) {
-      console.log(error);
-      setApiData(null);
-      setLoading(false);
+      if (error.response) {
+        const { status, data } = error.response;
+
+        if (
+          status === 404 ||
+          status === 403 ||
+          status === 500 ||
+          status === 302 ||
+          status === 409 ||
+          status === 401 ||
+          status === 400
+        ) {
+          console.log(error.response);
+          setApiData(null);
+          setOriginalData(null)
+          toast.error(data);
+          setLoading(false);
+        }
+      }
     }
   };
 
-  const handleArrayClick = (arrayName) => {
-    setSelectedArray(arrayName);
+  const handleArrayClick = async (value) => {
+    setFlagSend(value);
+    // setFlagGetData(true);
+    setCurrentPage(0);
   };
 
-  console.log(selectedArray);
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-  console.log(selectedArray);
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    setFlagGetData(true);
+  };
+
+  const goToPrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+      setFlagGetData(true);
+    }
+  };
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+    }, 700);
+
+    return () => clearTimeout(delay);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+    
+      try {
+        const payload = {
+          keyword: debouncedSearchTerm,
+        };
+
+        console.log("Payload", payload);
+
+        let response = await axios.post(
+          `${
+            import.meta.env.VITE_REACT_APP_BASE_URL
+          }/users/customer/get/searchData`,
+          payload
+        );
+
+        console.log(response.data);
+        setApiData(response.data);
+        setLoading(false);
+      } catch (error) {
+        if (error.response) {
+          const { status, data } = error.response;
+
+          if (
+            status === 404 ||
+            status === 403 ||
+            status === 500 ||
+            status === 302 ||
+            status === 409 ||
+            status === 401 ||
+            status === 400
+          ) {
+            console.log(error.response);
+            toast.error(data)
+            setLoading(false);
+          }
+        }
+      }
+    };
+
+    // Fetch data only if search term is not empty
+    if (debouncedSearchTerm !== "") {
+      fetchData();
+    } else {
+      setApiData(originalData);
+    }
+  }, [debouncedSearchTerm , originalData]);
+
+
+  // console.log("currentPage" , currentPage)
+  // console.log("MAx value " , maxData)
 
   return (
     <div className="overflow-x-auto">
@@ -42,34 +182,78 @@ const UserManagement = () => {
         <h1>Company Management</h1>
       </div>
 
+      <Toaster/>
+
       {loading ? (
         <p>Loading......</p>
       ) : (
         <div className="bg-white dark:bg-gray-800">
-          <nav className="max-w-screen-xl px-4 py-3 mx-auto">
-            <div className="flex items-center">
-              {apiData && (
-                <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-                  {Object.keys(apiData).map((arrayName, index) => (
-                    <li
-                      key={index}
-                      onClick={() => handleArrayClick(arrayName)}
-                      className={`text-gray-900 dark:text-white hover:underline cursor-pointer transition-colors duration-300 ease-in-out ${
-                        selectedArray === arrayName
-                          ? "bg-[#666666] text-white rounded-lg px-4 py-2"
-                          : ""
-                      }`}
-                    >
-                      {arrayName}
-                    </li>
+          <div className="bg-white dark:bg-gray-800">
+            <nav className="max-w-screen-xl px-4 py-3 mx-auto">
+              <div className="flex items-center justify-between">
+                <select
+                  value={flagSend}
+                  onChange={(e) => handleArrayClick(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-2 dark:bg-gray-700 "
+                >
+                  {companyDropDown.map((data) => (
+                    <option key={data.id} value={data.value}>
+                      {data.title}
+                    </option>
                   ))}
-                </ul>
-              )}
-            </div>
-          </nav>
-          {selectedArray && apiData && (
-            <UserNewComponent selectedArray={apiData[selectedArray]} />
-          )}
+                </select>
+                <div className=" w-[80%] xl:w-[45%]  justify-end ml-3">
+                  <div>
+                    <form className="flex items-center">
+                      <label htmlFor="simple-search" className="sr-only">
+                        Search
+                      </label>
+                      <div className="relative w-full">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                        <input
+                          type="text"
+                          placeholder="Search with User Name"
+                          id="camponeyorder"
+                          value={searchTerm}
+                          onChange={handleChange}
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+          {
+            apiData && (
+              <>
+                 <UserNewComponent selectedArray={apiData} />
+                 
+                <div className="flex  px-4 py-3">
+                  {currentPage !== 0 && (
+                    <button
+                      onClick={goToPrevPage}
+                      disabled={currentPage === 0 || loading}
+                      className="bg-gray-200 mr-5 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded"
+                    >
+                      Prev
+                    </button>
+                 )}
+
+                  {currentPage + 1 !== maxData && (
+                    <button
+                      onClick={goToNextPage}
+                      disabled={loading}
+                      className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded"
+                    >
+                      Next
+                    </button>
+                  )}
+                </div>
+              </>
+            )
+          }
         </div>
       )}
     </div>
--- nematAdmin/src/Component/sidebar/userMangement/UserNewComponent.jsx
+++ nematAdmin/src/Component/sidebar/userMangement/UserNewComponent.jsx
@@ -1,77 +1,75 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const UserNewComponent = ({selectedArray }) => {
+const UserNewComponent = ({ selectedArray }) => {
+  console.log("Inside UserComponet ", selectedArray);
 
-   console.log(selectedArray)
-      
+
   return (
-
-
     <div>
-         
-
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg py-2">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg py-2">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" className="px-6 py-3">
-                    Company Name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                     Company GST No.
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Company Number
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    status
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-                <th></th>
+              <th scope="col" className="px-6 py-3">
+                Company Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Company GST No.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Company Number
+              </th>
+              <th scope="col" className="px-6 py-3">
+                status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+              <th></th>
             </tr>
-        </thead>
-        <tbody>
-           {
-               selectedArray?.map((item) => (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={item._id}>
-                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {item.CompanyName}
-                     </th>
-                     <td className="px-6 py-4">
-                        {item.GstNo}
-                     </td>
-                     <td className="px-6 py-4">
-                        {item.MobileNumber}
-                     </td>
-                     <td className="px-6 py-4 ">
-                        <div className='ml-4'>
-                           {item.status === 1 ? (
-                                 <div className="h-3 w-3 rounded-full bg-green-500 me-2"></div> 
-                           ) : (
-                              <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> 
-                           )}
-                        </div>
-                        
-                     </td>
-                     
-                     <td className="px-6 py-4" >
-                        <Link to={`/dashboard/user-mangement/user_prev/${item._id}`}>
-                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Prev</a>
-                        </Link>
-                     </td>
-                  </tr>
-               ))
-            }
-            
-        </tbody>
-    </table>
-</div>
+          </thead>
+          <tbody>
+            {selectedArray?.map((item) => (
+              <tr
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                key={item._id}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {item.CompanyName}
+                </th>
+                <td className="px-6 py-4">{item.GstNo}</td>
+                <td className="px-6 py-4">{item.MobileNumber}</td>
+                <td className="px-6 py-4 ">
+                  <div className="">
+                    {item.status === 1 ? (
+                      <p className="bg-green-500 p-2 text-white text-center rounded-lg">Active</p>
+                    ) : (
+                      <p  className="bg-blue-500 p-2 text-white text-center rounded-lg">InActive</p>
+                    )}
+                  </div>
+                </td>
 
+                <td className="px-6 py-4">
+                  <Link to={`/dashboard/user-mangement/user_prev/${item._id}`}>
+                    <a
+                      href="#"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Prev
+                    </a>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserNewComponent
+export default UserNewComponent;
