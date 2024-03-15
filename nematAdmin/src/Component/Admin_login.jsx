e442153e173bdd279866c1dc5a454c531a153cc1
raj22001 <rajmahajan960@gmail.com>
2024-03-15 15:49:33 +0530

    Login Name Change To Admin Log In

--- nematAdmin/src/Component/Admin_login.jsx
+++ nematAdmin/src/Component/Admin_login.jsx
@@ -95,7 +95,7 @@ const  Admin_login = () => {
         </div>
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Log in</h2>
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Admin Log in</h2>
             <form  className="mt-8" onSubmit={handleSubmit}>
               <div className="space-y-5">
                 <div>
