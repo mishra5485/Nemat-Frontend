24a55723a3a15381be9d6d867207e6bcf5ceb509
raj22001 <rajmahajan960@gmail.com>
2024-03-16 17:03:56 +0530

    Admin Login Image Change

--- nematAdmin/src/Component/Admin_login.jsx
+++ nematAdmin/src/Component/Admin_login.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAddmin } from "../slice/AdminProfileSlice";
 import { useNavigate } from "react-router-dom";
-import loginImage from "../assets/loginImage.png"
+import loginImage from "../assets/loginImage.jpg"
 
 const  Admin_login = () => {
    
