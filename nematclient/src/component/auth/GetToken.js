ee9d28a9fc13b62928178df0600346c250778b52
raj22001 <rajmahajan960@gmail.com>
2024-03-19 11:34:31 +0530

    unwanted Image Remove

--- nematclient/src/component/auth/GetToken.js
+++ nematclient/src/component/auth/GetToken.js
@@ -1,7 +1,7 @@
 const getToken = () => {
   const token = localStorage.getItem("token");
 
-  console.log("token ", token);
+  // console.log("token ", token);
 
   const config = {
     headers: { Authorization: token },
