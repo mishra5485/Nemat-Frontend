5c342314d042029eb65575021b4f48897e3ce47f
raj22001 <rajmahajan960@gmail.com>
2024-03-27 10:43:50 +0530

    RoadMap SVG Added In background

--- nematclient/src/pages/AboutUs.jsx
+++ nematclient/src/pages/AboutUs.jsx
@@ -8,6 +8,7 @@ import Footer from "../component/footer/footer";
 import ProductHeader from "../component/common/ProductHeader";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import getToken from "../component/auth/GetToken";
+import RoadMapImage from "../assets/HomePage/Vector 30.png";
 
 const AboutUs = () => {
   const [aboutUsData, setAboutUsData] = useState([]);
@@ -32,11 +33,9 @@ const AboutUs = () => {
     AboutUsData();
   }, []);
 
-  
   const AboutUsData = async () => {
     try {
-
-      const header = getToken()
+      const header = getToken();
 
       let response = await axios.get(
         `${import.meta.env.VITE_REACT_APP_BASE_URL}/aboutus/getData`,
@@ -100,18 +99,18 @@ const AboutUs = () => {
   };
 
   const checkHandler = () => {
-    if(roadmapModal){
-      setRoadMapModal(false)
+    if (roadmapModal) {
+      setRoadMapModal(false);
     }
-  }
+  };
 
   return (
-    <div >
+    <div>
       <NavBars />
       {loading ? (
         <p></p>
       ) : (
-        <div id="mainSection" className="aboutSection" >
+        <div id="mainSection" className="aboutSection">
           <div className="heroSection">
             <div className="banner">
               <img
@@ -140,82 +139,102 @@ const AboutUs = () => {
           </div>
 
           <div className="w-full bg-Cream">
-          <div className="w-[90%] mx-auto mobile:mt-4 mobile:mb-4 h-auto z-10 bg-Cream md:flex md:justify-between relative"  >
-            {aboutUsData.RoadMapData?.map((roadmapdata, index) => (
-              <div
-                key={index}
-                className={`flex w-full mt-[10%] ${index % 2 === 0 ? 'mobile:justify-start sm:justify-start md:justify-between' : 'mobile:justify-end sm:justify-end md:justify-between'}`}
-                style={{
-                  marginTop: getMarginTop(index),
-                  ...(window.innerWidth <= 740 && { marginTop: "20px" }),
-                }}
-              >
-                <div onClick={() => handleModalOpen(index)} className="">
-                  <img
-                    src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/${
-                      roadmapdata?.ImagePath
-                    }`}
-                    alt={`Roadmap Image ${index}`}
-                    className="w-[250px] h-[250px] object-contain hover:scale-110 transition-transform duration-500 hover:z-50 "
-                  />
-                  <p className="text-center mt-2 font-roxborough text-text_Color text-2xl font-bold">
-                    {roadmapdata.Heading}
-                  </p>
-                  <p className="text-center font-Marcellus text-text_Color">
-                    {roadmapdata.Year}
-                  </p>
-                  <p className="text-center underline uppercase font-semibold mt-2 font-Marcellus text-text_Color2 cursor-pointer">
-                    know more
-                  </p>
-                </div>
+            <div className="w-[90%] mx-auto mobile:mt-4 mobile:mb-4 h-auto z-10 bg-Cream md:flex md:justify-between relative">
+              {/* Add the SVG background image here */}
+              <div className="absolute inset-0 ">
+                <img
+                  src={RoadMapImage}
+                  alt="RoadMap Background"
+                  className="w-[90%] flex justify-center h-full object-contain z-20"
+                />
               </div>
-            ))}
+              {/* End of SVG background */}
+
+              {/* Your road map items */}
+              {aboutUsData.RoadMapData?.map((roadmapdata, index) => (
+                <div
+                  key={index}
+                  className={`flex w-full mt-[10%] ${
+                    index % 2 === 0
+                      ? "mobile:justify-start sm:justify-start md:justify-between"
+                      : "mobile:justify-end sm:justify-end md:justify-between"
+                  }`}
+                  style={{
+                    marginTop: getMarginTop(index),
+                    ...(window.innerWidth <= 740 && { marginTop: "20px" }),
+                  }}
+                >
+                  <div onClick={() => handleModalOpen(index)} className="">
+                    <img
+                      src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/${
+                        roadmapdata?.ImagePath
+                      }`}
+                      alt={`Roadmap Image ${index}`}
+                      className="w-[250px] h-[250px] object-contain hover:scale-110 transition-transform duration-500 hover:z-50 z-30"
+                    />
+                    <p className="text-center mt-2 font-roxborough text-text_Color text-2xl font-bold">
+                      {roadmapdata.Heading}
+                    </p>
+                    <p className="text-center font-Marcellus text-text_Color">
+                      {roadmapdata.Year}
+                    </p>
+                    <p className="text-center underline uppercase font-semibold mt-2 font-Marcellus text-text_Color2 cursor-pointer">
+                      know more
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-          </div>   
 
           {/* RoadMapModal open Section */}
           {roadmapModal && (
             <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 flex items-center justify-center z-50">
               <div className="bg-white p-4 rounded-md mobile:w-full mobile:h-[80%] md:w-[400px] md:h-[95%] lg:h-[40rem]">
                 <div className="h-[92%]">
-                {roadmapdataModal.map((data, index) => (
-                  <div
-                    key={index}
-                    className={index === currentIndex ? "" : "hidden"}
-                    style={{
-                      opacity: index === currentIndex ? 1 : 0,
-                      transition: "opacity 1s ease-in-out",
-                      visibility: index === currentIndex ? "visible" : "hidden",
-                    }}
-                  >
-                    <div className="flex justify-between mt-4 mobile:w-full">
-                      <button type="button" onClick={handlePrev} className="mobile:w-[5%]" >
-                        <FaChevronLeft size={25} />
-                      </button>
-                      <div className="md:w-[60%] md:h-[35%] mobile:w-[80%] md:m-auto md:mt-[3%] flex">
-                        <img
-                          src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/${
-                            data.ImagePath
-                          }`}
-                          className="w-full h-full object-contain"
-                          alt={`Image ${index}`}
-                        />
+                  {roadmapdataModal.map((data, index) => (
+                    <div
+                      key={index}
+                      className={index === currentIndex ? "" : "hidden"}
+                      style={{
+                        opacity: index === currentIndex ? 1 : 0,
+                        transition: "opacity 1s ease-in-out",
+                        visibility:
+                          index === currentIndex ? "visible" : "hidden",
+                      }}
+                    >
+                      <div className="flex justify-between mt-4 mobile:w-full">
+                        <button
+                          type="button"
+                          onClick={handlePrev}
+                          className="mobile:w-[5%]"
+                        >
+                          <FaChevronLeft size={25} />
+                        </button>
+                        <div className="md:w-[60%] md:h-[35%] mobile:w-[80%] md:m-auto md:mt-[3%] flex">
+                          <img
+                            src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/${
+                              data.ImagePath
+                            }`}
+                            className="w-full h-full object-contain"
+                            alt={`Image ${index}`}
+                          />
+                        </div>
+                        <button onClick={handleNext} className="mobile:w-[5%]">
+                          <FaChevronRight size={25} />
+                        </button>
                       </div>
-                      <button onClick={handleNext} className="mobile:w-[5%]">
-                        <FaChevronRight size={25} />
-                      </button>
+                      <div className="w-full text-lg mt-3">
+                        <ProductHeader
+                          className="text-xl md:w-[90%] mx-auto"
+                          title={data.Heading}
+                        />
+                        <p className="md:w-[98%]   text-base mt-2 font-roxborough text-text_Color font-semibold mx-auto text-center">
+                          {data.Description}
+                        </p>
+                      </div>
                     </div>
-                    <div className="w-full text-lg mt-3">
-                      <ProductHeader
-                        className="text-xl md:w-[90%] mx-auto"
-                        title={data.Heading}
-                      />
-                      <p className="md:w-[98%]   text-base mt-2 font-roxborough text-text_Color font-semibold mx-auto text-center">
-                        {data.Description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
+                  ))}
                 </div>
                 <p
                   onClick={() => setRoadMapModal(false)}
